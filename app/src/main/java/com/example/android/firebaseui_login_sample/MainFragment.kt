1988a9cdb3fd4d5fdbf38b63d615b202ba6738c3
Evan Greer <evan.greer@iterable.com>
2022-10-04 19:56:09 -0600

    sets up UI changes for login and logout by observing authentication state

--- app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
+++ app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
@@ -49,10 +49,6 @@ class MainFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
 
-        // TODO Remove the two lines below once observeAuthenticationState is implemented.
-        binding.welcomeText.text = viewModel.getFactToDisplay(requireContext())
-        binding.authButton.text = getString(R.string.login_btn)
-
         return binding.root
     }
 
@@ -90,16 +86,35 @@ class MainFragment : Fragment() {
     private fun observeAuthenticationState() {
         val factToDisplay = viewModel.getFactToDisplay(requireContext())
 
-        // TODO Use the authenticationState variable from LoginViewModel to update the UI
-        //  accordingly.
-        //
-        //  TODO If there is a logged-in user, authButton should display Logout. If the
-        //   user is logged in, you can customize the welcome message by utilizing
-        //   getFactWithPersonalition(). I
-
         // TODO If there is no logged in user, authButton should display Login and launch the sign
         //  in screen when clicked. There should also be no personalization of the message
         //  displayed.
+
+        // use the authenticationState variable from LoginViewModel to update UI accordingly
+        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            when (authenticationState) {
+                // if user is logged in
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    // authButton should display logout
+                    binding.authButton.text = getString(R.string.logout_button_text)
+                    binding.authButton.setOnClickListener {
+                        AuthUI.getInstance().signOut(requireContext())
+                    }
+
+                    // you can customize the welcome message they see by
+                    // utilizing the getFactWithPersonalization() function provided
+                    binding.welcomeText.text = getFactWithPersonalization(factToDisplay)
+
+                }
+                else -> {
+                    // if there is no logged-in user, auth_button should display Login and
+                    // launch the sign in screen when clicked.
+                    binding.authButton.text = getString(R.string.login_button_text)
+                    binding.authButton.setOnClickListener { launchSignInFlow() }
+                    binding.welcomeText.text = factToDisplay
+                }
+            }
+        })
     }
 
 
